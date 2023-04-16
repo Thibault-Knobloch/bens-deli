934c42fef643351b67d0f6059c9e32347afbc97e
Thibault-Knobloch <thibaultknobloch@yahoo.fr>
2023-04-16 18:00:32 +0200

    fix failing test after migration

--- bensdeli/products/tests/test_views.py
+++ bensdeli/products/tests/test_views.py
@@ -1,16 +1,18 @@
 from products.views import calculate_average_rating
 from products.models import Review, Product
+from django.contrib.auth.models import User
 
 import pytest
 
 @pytest.mark.django_db
 def test_calculate_average_review_rating_works():
     # GIVEN
+    user = User.objects.create_user(username='testuser', password='testpass')
     product = Product.objects.create(name="Test Product", desc="Test Desc", price=2.00, spice_level=Product.Spice.MILD)
-    Review.objects.create(name="Reviewer 1", rating=2, product=product)
-    Review.objects.create(name="Reviewer 2", rating=2, product=product)
-    Review.objects.create(name="Reviewer 3", rating=3, product=product)
-    Review.objects.create(name="Reviewer 4", rating=4, product=product)
+    Review.objects.create(user=user, rating=2, product=product)
+    Review.objects.create(user=user, rating=2, product=product)
+    Review.objects.create(user=user, rating=3, product=product)
+    Review.objects.create(user=user, rating=4, product=product)
 
     #WHEN
     reviews = product.reviews.all()
